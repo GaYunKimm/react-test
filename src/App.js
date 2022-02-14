f6b0630be1925fbab280fb84df145f531fec7449
gayun <gayoon109@gmail.com>
2022-02-15 02:17:30 +0900

    리액트 state, props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react/cjs/react.production.min';
+import TOC from './components/TOC';
+import Subject from './components/Subject';
+import Content from './components/Content';
+
+
+
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: 'webs', sub: 'world wide web입니다' },
+      toc: [
+        { id: 1, title: 'HTML', desc: 'html is for information' },
+        { id: 2, title: 'CSS', desc: 'css is for design' },
+        { id: 3, title: 'JavaScript', desc: 'js is for interaction' },
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        Hello, react!!
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.toc}></TOC>
+        <Content></Content>
       </div>
     );
   }
---
+++ src/components/Content.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+//import './App.css';
+
+class Content extends Component {
+
+
+  render() {
+    return (
+      <article>
+        <h2>html</h2>
+        html is hyperText Markup language.
+      </article>
+    );
+  }
+}
+
+
+export default Content;
